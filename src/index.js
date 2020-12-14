f93ccb7b0ab16fe92f19a19fe9e0f236bc97bd76
parksohyeon <dso753@elinmedia.com>
2020-12-15 00:23:24 +0900

    Recap Refactor

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 number.innerText = 0;
 
 const counterModifier = (count = 0, action) => {
-    if (action.type === 'ADD') {
-        return count + 1;
-    }
-    else if (action.type === 'MINUS') {
-        return count - 1;
-    }
-    else {
-        return count;
+    switch (action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            return count;
     }
 };
 
@@ -26,5 +28,5 @@ const onChange = () => {
 
 counterStore.subscribe(onChange);
 
-add.addEventListener('click', () => counterStore.dispatch({ type: 'ADD' }));
-minus.addEventListener('click', () => counterStore.dispatch({ type: 'MINUS' }));
+add.addEventListener('click', () => counterStore.dispatch({ type: ADD }));
+minus.addEventListener('click', () => counterStore.dispatch({ type: MINUS }));
