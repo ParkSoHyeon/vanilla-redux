6639d3cef6a367d30de018c0e51f1d474440751c
parksohyeon <dso753@elinmedia.com>
2020-12-15 00:11:41 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const counterModifier = (state = 0) => {
-    return state;
+const counterModifier = (count = 0, action) => {
+    if (action.type === 'ADD') {
+        return count + 1;
+    }
+    else if (action.type === 'MINUS') {
+        return count - 1;
+    }
+    else {
+        return count;
+    }
 };
 
 const counterStore = createStore(counterModifier);
+
+counterStore.dispatch({ type: 'ADD' });
+counterStore.dispatch({ type: 'ADD' });
+counterStore.dispatch({ type: 'ADD' });
+counterStore.dispatch({ type: 'ADD' });
+counterStore.dispatch({ type: 'ADD' });
+counterStore.dispatch({ type: 'ADD' });
+counterStore.dispatch({ type: 'MINUS' });
+
 console.log(counterStore.getState());
