1dd639d1d10f53b65bd86579f87f0a06c5df5d74
parksohyeon <dso753@elinmedia.com>
2020-12-15 00:37:57 +0900

    Vanilla ToDo

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
-import { createStore } from 'redux';
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
-
-const ADD = 'ADD';
-const MINUS = 'MINUS';
-
-number.innerText = 0;
-
-const counterModifier = (count = 0, action) => {
-    switch (action.type) {
-        case ADD:
-            return count + 1;
-        case MINUS:
-            return count - 1;
-        default:
-            return count;
-    }
-};
-
-const counterStore = createStore(counterModifier);
-
-const onChange = () => {
-    number.innerText = counterStore.getState();
+const createToDo = toDo => {
+    const li = document.createElement('li');
+    li.innerText = toDo;
+    ul.appendChild(li);
 }
 
-counterStore.subscribe(onChange);
+const onSubmit = e => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = '';
+    createToDo(toDo);
+}
 
-add.addEventListener('click', () => counterStore.dispatch({ type: ADD }));
-minus.addEventListener('click', () => counterStore.dispatch({ type: MINUS }));
+form.addEventListener('submit', onSubmit);
